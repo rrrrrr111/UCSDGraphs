afdf04c490b30672deaf36b88956a98562145776
Roman Churganov <RIChurganov.SBT@sberbank.ru>
2017-07-17 22:00:35 +0300

    Code review

--- src/roadgraph/MapGraph.java
+++ src/roadgraph/MapGraph.java
@@ -10,7 +10,14 @@ package roadgraph;
 import geography.GeographicPoint;
 import util.GraphLoader;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
 import java.util.function.Consumer;
 
 import static java.lang.Double.compare;
@@ -204,6 +211,8 @@ public class MapGraph {
         // Dummy variable for calling the search algorithms
         // You do not need to change this method.
         Consumer<GeographicPoint> temp = (x) -> {
+
+            System.out.println("DIJKSTRA visiting [" + x + "] intersects streets: " + ((Crossroad)x).getOutRoads());
         };
         return dijkstra(start, goal, temp);
     }
@@ -219,10 +228,10 @@ public class MapGraph {
      */
     public List<GeographicPoint> dijkstra(GeographicPoint start,
                                           GeographicPoint goal, Consumer<GeographicPoint> visualiser) {
-        Map<DistancedFromStartCrossroad, DistancedFromStartCrossroad> fromTo = new HashMap<>();
-        PriorityQueue<DistancedFromStartCrossroad> queue = new PriorityQueue<>();
+        Map<PriorityCrossroad, PriorityCrossroad> fromTo = new HashMap<>();
+        PriorityQueue<PriorityCrossroad> queue = new PriorityQueue<>();
 
-        DistancedFromStartCrossroad current = new DistancedFromStartCrossroad(find(start), 0);
+        PriorityCrossroad current = new PriorityCrossroad(find(start), 0);
         Crossroad goalCrossroad = find(goal);
 
         queue.add(current);
@@ -235,33 +244,37 @@ public class MapGraph {
                 break;
             }
 
-            double distanceFromStart = current.getDistanceFromStart();
+            double distanceFromStart = current.getDistance();
             for (Road outRoad : current.getOutRoads()) {
-                DistancedFromStartCrossroad neighbour = new DistancedFromStartCrossroad(
+                PriorityCrossroad neighbour = new PriorityCrossroad(
                         outRoad.getToCrossroad(), distanceFromStart + outRoad.getLength()
                 );
 
-
-                if (!fromTo.containsKey(neighbour)) {
-                    fromTo.put(neighbour, current);
-                    queue.add(neighbour);
-
-                } else {
-                    for (Map.Entry<DistancedFromStartCrossroad, DistancedFromStartCrossroad> entry : fromTo.entrySet()) {
-
-                        DistancedFromStartCrossroad previousNeighbour = entry.getKey();
-
-                        if (previousNeighbour.equals(neighbour)
-                                && neighbour.closerToStartThan(previousNeighbour)) {
-
-                            fromTo.put(neighbour, current);
-                            queue.add(neighbour);
-                        }
-                    }
-                }
+                checkNeighbour(fromTo, queue, current, neighbour);
             }
         }
         return buildPath(start, goal, fromTo);
+    }
+
+    private void checkNeighbour(Map<PriorityCrossroad, PriorityCrossroad> fromTo, PriorityQueue<PriorityCrossroad> queue, PriorityCrossroad current, PriorityCrossroad neighbour) {
+        if (!fromTo.containsKey(neighbour)) {
+            fromTo.put(neighbour, current);
+            queue.add(neighbour);
+
+        } else {
+            for (Map.Entry<PriorityCrossroad, PriorityCrossroad> entry : fromTo.entrySet()) {
+
+                PriorityCrossroad previousNeighbour = entry.getKey();
+
+                if (previousNeighbour.equals(neighbour)
+                        && neighbour.hasHigherPriorityThan(previousNeighbour)) {
+
+                    fromTo.put(neighbour, current);
+                    queue.add(neighbour);
+                    break;
+                }
+            }
+        }
     }
 
     /**
@@ -275,6 +288,8 @@ public class MapGraph {
     public List<GeographicPoint> aStarSearch(GeographicPoint start, GeographicPoint goal) {
         // Dummy variable for calling the search algorithms
         Consumer<GeographicPoint> temp = (x) -> {
+
+            System.out.println("A* visiting [" + x + "] intersects streets: " + ((Crossroad)x).getOutRoads());
         };
         return aStarSearch(start, goal, temp);
     }
@@ -282,20 +297,47 @@ public class MapGraph {
     /**
      * Find the path from start to goal using A-Star search
      *
-     * @param start        The starting location
-     * @param goal         The goal location
-     * @param nodeSearched A hook for visualization.  See assignment instructions for how to use it.
+     * @param start      The starting location
+     * @param goal       The goal location
+     * @param visualiser A hook for visualization.  See assignment instructions for how to use it.
      * @return The list of crossroads that form the shortest path from
      * start to goal (including both start and goal).
      */
     public List<GeographicPoint> aStarSearch(GeographicPoint start,
-                                             GeographicPoint goal, Consumer<GeographicPoint> nodeSearched) {
-        // TODO: Implement this method in WEEK 4
+                                             GeographicPoint goal, Consumer<GeographicPoint> visualiser) {
+        Map<PriorityCrossroad, PriorityCrossroad> fromTo = new HashMap<>();
+        PriorityQueue<PriorityCrossroad> queue = new PriorityQueue<>();
 
-        // Hook for visualization.  See writeup.
-        //nodeSearched.accept(next.getLocation());
+        PriorityCrossroad current = new PriorityCrossroad(find(start), 0);
+        Crossroad goalCrossroad = find(goal);
 
-        return null;
+        queue.add(current);
+
+        for (; ; ) {
+            current = queue.poll();
+            visualiser.accept(current);
+
+            if (current.equals(goalCrossroad)) {
+                break;
+            }
+
+            double distanceFromStart = current.getDistance();
+            for (Road outRoad : current.getOutRoads()) {
+                PriorityCrossroad neighbour = new PriorityCrossroad(
+                        outRoad.getToCrossroad(), distanceFromStart + outRoad.getLength() +
+                        calculateLengthBetween(outRoad.getToCrossroad(), goalCrossroad)
+                );
+
+
+                checkNeighbour(fromTo, queue, current, neighbour);
+            }
+        }
+        return buildPath(start, goal, fromTo);
+    }
+
+    private static double calculateLengthBetween(Crossroad fromCrossroad, Crossroad toCrossroad) {
+        return Math.abs(fromCrossroad.getX() - toCrossroad.getX())
+                + Math.abs(fromCrossroad.getY() - toCrossroad.getY());
     }
 
     @Override
@@ -309,6 +351,28 @@ public class MapGraph {
                     .append("]\n");
         }
         return sb.append("]\n").toString();
+    }
+
+    private static class PriorityCrossroad extends Crossroad implements Comparable<PriorityCrossroad> {
+        private final double distance;
+
+        PriorityCrossroad(Crossroad crossroad, double distance) {
+            super(crossroad);
+            this.distance = distance;
+        }
+
+        private double getDistance() {
+            return distance;
+        }
+
+        private boolean hasHigherPriorityThan(PriorityCrossroad crossroad) {
+            return compareTo(crossroad) < 0;
+        }
+
+        @Override
+        public int compareTo(PriorityCrossroad o) {
+            return compare(distance, o.distance);
+        }
     }
 
     public static void main(String[] args) {
@@ -333,8 +397,11 @@ public class MapGraph {
         GeographicPoint testEnd = new GeographicPoint(8.0, -1.0);
 
         System.out.println("Test 1 using simpletest: Dijkstra should be 9 and AStar should be 5");
-        List<GeographicPoint> testroute = simpleTestMap.dijkstra(testStart, testEnd);
-        List<GeographicPoint> testroute2 = simpleTestMap.aStarSearch(testStart, testEnd);
+
+        List<GeographicPoint> result = simpleTestMap.dijkstra(testStart, testEnd);
+        check(result, 9);
+        result = simpleTestMap.aStarSearch(testStart, testEnd);
+        check(result, 5);
 
 
         MapGraph testMap = new MapGraph();
@@ -344,16 +411,20 @@ public class MapGraph {
         testStart = new GeographicPoint(32.869423, -117.220917);
         testEnd = new GeographicPoint(32.869255, -117.216927);
         System.out.println("Test 2 using utc: Dijkstra should be 13 and AStar should be 5");
-        testroute = testMap.dijkstra(testStart, testEnd);
-        testroute2 = testMap.aStarSearch(testStart, testEnd);
+        result = testMap.dijkstra(testStart, testEnd);
+        check(result, 13);
+        result = testMap.aStarSearch(testStart, testEnd);
+        check(result, 5);
 
 
         // A slightly more complex test using real data
         testStart = new GeographicPoint(32.8674388, -117.2190213);
         testEnd = new GeographicPoint(32.8697828, -117.2244506);
         System.out.println("Test 3 using utc: Dijkstra should be 37 and AStar should be 10");
-        testroute = testMap.dijkstra(testStart, testEnd);
-        testroute2 = testMap.aStarSearch(testStart, testEnd);
+        result = testMap.dijkstra(testStart, testEnd);
+        check(result, 37);
+        result = testMap.aStarSearch(testStart, testEnd);
+        check(result, 10);
 
 
 		/* Use this code in Week 3 End of Week Quiz */
@@ -365,30 +436,13 @@ public class MapGraph {
         GeographicPoint start = new GeographicPoint(32.8648772, -117.2254046);
         GeographicPoint end = new GeographicPoint(32.8660691, -117.217393);
 
-
         List<GeographicPoint> route = theMap.dijkstra(start, end);
         List<GeographicPoint> route2 = theMap.aStarSearch(start, end);
     }
 
-    private static class DistancedFromStartCrossroad extends Crossroad implements Comparable<DistancedFromStartCrossroad> {
-        private final double distanceFromStart;
-
-        DistancedFromStartCrossroad(Crossroad crossroad, double distanceFromStart) {
-            super(crossroad);
-            this.distanceFromStart = distanceFromStart;
-        }
-
-        private double getDistanceFromStart() {
-            return distanceFromStart;
-        }
-
-        private boolean closerToStartThan(DistancedFromStartCrossroad crossroad) {
-            return this.distanceFromStart < crossroad.getDistanceFromStart();
-        }
-
-        @Override
-        public int compareTo(DistancedFromStartCrossroad o) {
-            return compare(distanceFromStart, o.distanceFromStart);
+    private static void check(List<GeographicPoint> result, int resultSize) {
+        if (result.size() != resultSize) {
+            throw new RuntimeException("The algorithm should give " + resultSize + ", but gave: " + result.size() + ", result path:" + result);
         }
     }
 }
